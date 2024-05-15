c8c6caab82976e052727be33e1abf1c5040d0c6b
Leyos <Romdz01@gmail.com>
2024-05-15 09:47:03 +0200

    feat(Variables.js)rename 1 to variables

---
+++ Fondamentaux_JS/Variables.js
@@ -0,0 +1,14 @@
+
+/*Déclarez deux variables : admin and name.
+Assignez la valeur "John" à name.
+Copiez la valeur de name à admin.
+Afficher la valeur de admin en utilisant alert (devrait afficher “John”). */
+
+let admin;
+
+let name;
+
+name = "john"
+admin = name
+
+alert(admin)
