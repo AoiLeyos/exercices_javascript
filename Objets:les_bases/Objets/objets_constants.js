79662a2a09be090d88d2f0880fb8a4d603d030e1
Leyos <Romdz01@gmail.com>
2024-05-17 09:06:40 +0200

    feat(objets_constants.js) exercises

---
+++ Objets:les_bases/Objets/objets_constants.js
@@ -0,0 +1,12 @@
+/*Objets constants ?
+importance: 5
+
+Est-il possible de changer un objet déclaré avec const, comment ?*/
+`const user = {
+    name: "John"
+  };
+  
+  // est-ce que ça fonctionne ?
+  user.name = "Pete";`
+
+// l'objet ne peut pas changer mais son contenu le peut 
