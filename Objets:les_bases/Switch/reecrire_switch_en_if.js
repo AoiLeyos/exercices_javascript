74f990486d4a9677050e93bf2ce2d934118f7a7a
Leyos <Romdz01@gmail.com>
2024-05-21 09:16:16 +0200

    feat(reecrire_switch_en_if.js) exercises

---
+++ Objets:les_bases/Switch/reecrire_switch_en_if.js
@@ -0,0 +1,10 @@
+'use strict'
+
+ let browser = prompt("Enter a browser's name :");
+  if (browser === 'Edge') {
+    console.log('You\'ve got the Edge!');
+  } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    console.log('Okay we support these browsers too');
+  } else {
+    console.log('We hope that this page looks ok!');
+  }
